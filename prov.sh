abb1253725e23da5a93b6261f1b19b82517156db
Eric Kennedy van Dam <jiefka@gmail.com>
2026-02-22 10:37:36 +0100

    variables

--- prov.sh
+++ prov.sh
@@ -6,16 +6,18 @@ echo
 if [ "$(id -u)" != "0" ]; then
   echo "Error: Ce script doit être exécuté avec sudo."
   echo "Veuillez utiliser la commande suivante : "
-  echo "wget -q -O - http://ansible.lan.ekla-danse.be/scripts/prov.sh | sudo bash"
+  echo "source < (wget -O env -q http://ansible.lan.ekla-danse.be/scripts/prov.sh) && wget -q -O - http://ansible.lan.ekla-danse.be/scripts/prov.sh | sudo bash"
   exit 1
 fi
 
+echo ${SEMAPHORE_PWD}
+echo ${SEM_SSH_KEY}
 # Créer un compte pour semaphore
 echo "Création de l'utilisateur semaphore"
 if ( ! id semaphore &> /dev/null )
 then
 useradd -r -s /bin/bash -m semaphore
-usermod -p "SEMAPHORE_PWD" semaphore # voir fichier env
+usermod -p "${SEMAPHORE_PWD}" semaphore # voir fichier env
 else
 echo "Utilisateur déja présent."
 fi
@@ -29,7 +31,7 @@ chown semaphore:semaphore /home/semaphore/.ssh
 chmod 700 /home/semaphore/.ssh
 fi
 
-SEM_SSH_KEY="" # voir fichier env
+# SEM_SSH_KEY="" # voir fichier env
 if ( ! grep "${SEM_SSH_KEY}" /home/semaphore/.ssh/authorized_keys &> /dev/null );
 then
 echo "${SEM_SSH_KEY}" >> /home/semaphore/.ssh/authorized_keys
